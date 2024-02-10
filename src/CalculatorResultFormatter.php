cfee5efc7fb0dc859beb41daa4eb6dc45eed190b
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-02-10 19:36:01 +0100

    Corrected DocBlock and rename variable

--- src/CalculatorResultFormatter.php
+++ src/CalculatorResultFormatter.php
@@ -12,18 +12,18 @@ class CalculatorResultFormatter
   /**
    * Formats mathematical calculation results into an XML string.
    *
-   * @param string $numberType The type of mathematical operation performed (e.g., "addition", "subtraction").
-   * @param array $numbers        An array of numbers used in the calculation.
+   * @param string $numbersType The type of numbers to be placed in the parent element (e.g. "primeNumbers").
+   * @param array $numbers An array of numbers to be placed in the child elements.
    *
    * @return string The formatted XML string representing the calculation result.
    */
-  public function toXML(string $numberType, array $numbers): string
+  public function toXML(string $numbersType, array $numbers): string
   {
     if (empty($numbers)) {
       throw new FormatterInputException('Input array cannot be empty');
     }
 
-    $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><' . $numberType . '/>');
+    $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><' . $numbersType . '/>');
 
     $xml->addAttribute('amount', count($numbers));
     $result = $xml->addChild('result');
