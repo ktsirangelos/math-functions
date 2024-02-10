22dfeb37984d4acb4cab75a11303273e0addaa18
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-02-10 23:45:19 +0100

    Add strict typing

--- src/CalculatorResultFormatter.php
+++ src/CalculatorResultFormatter.php
@@ -1,8 +1,10 @@
 <?php
 
+/* declare(strict_types=1); */
+
 namespace MathFunctions;
 
-use MathFunctions\FormatterInputException;
+use MathFunctions\Exceptions\FormatterInputException;
 
 /**
  * Provides XML formatting functionality for mathematical results.
@@ -26,13 +28,16 @@ class CalculatorResultFormatter
 
     $dom = new \DOMDocument('1.0', 'UTF-8');
 
+    // Create the root element
     $root = $dom->createElement($numbersType);
     $root->setAttribute('amount', count($numbers));
     $dom->appendChild($root);
 
+    // Create the 'result' element
     $resultNode = $dom->createElement('result');
     $root->appendChild($resultNode);
 
+    // Add number elements to the 'result' node
     foreach ($numbers as $number) {
       $numberNode = $dom->createElement('number', $number);
       $resultNode->appendChild($numberNode);
@@ -41,3 +46,4 @@ class CalculatorResultFormatter
     return $dom->saveXML();
   }
 }
+
--- src/IntegerCalculator.php
+++ src/IntegerCalculator.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MathFunctions;
 
 use MathFunctions\Exceptions\CalculatorInputException;
-use MathFunctions\Exceptions\CalculatorResultFormatter;
+use MathFunctions\CalculatorResultFormatter;
 
 /**
  * A class providing various integer-based mathematical operations.
